784bdd36c858128ba16e8ad0a74b40bf52774d32
alexandr50 <dmitrtymaslov2016@mail.ru>
2022-03-26 19:27:48 +0300

    homework for lesson_2

--- geekshop/geekshop/mainapp/views.py
+++ geekshop/geekshop/mainapp/views.py
@@ -1,8 +1,29 @@
+import json
+import os
 from django.shortcuts import render
+from datetime import datetime
 
+
+now = datetime.today().strftime('%H:%M')
 # Create your views here.
+MODULE_DIR = os.path.dirname(__file__)
+
+def read_json(file):
+    file_path = os.path.join(MODULE_DIR, file)
+    return json.load(open(file_path, encoding='utf-8'))
+
+
 def index(request):
-    return render(request, 'mainapp/index.html')
+    content = {'title': 'GeekShop',
+               'time': now}
+
+    return render(request, 'mainapp/index.html', content)
+
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    content = {'title': 'GeekShop - Каталог',
+               'categories': read_json('fixtures/categories.json'),
+               'products': read_json('fixtures/goods.json'),
+               'time': now}
+
+    return render(request, 'mainapp/products.html', content)
